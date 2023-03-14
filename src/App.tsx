e33a8a88d1ecb8b601a961216093d7b6268e704b
baljeet0531 <barryfang0531@gmail.com>
2023-03-14 16:26:23 +0800

    feat(game-scene): create scene with tilemap and add collider between player and layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Center, Box } from "@chakra-ui/react";
+import React, { useEffect, useRef } from "react";
 
-function App() {
+import SceneMatter from "./Components/SceneMatter";
+
+window.onload = function () {
+  function resize() {
+    let canvas = document.querySelector("canvas");
+    let ww = window.innerWidth;
+    let wh = window.innerHeight;
+    let wRatio = ww / wh;
+    let gameRatio = (config.width as number) / (config.height as number);
+
+    if (canvas) {
+      if (wRatio < gameRatio) {
+        canvas.style.width = ww + "px";
+        canvas.style.height = ww / gameRatio + "px";
+      } else {
+        canvas.style.width = wh * gameRatio + "px";
+        canvas.style.height = wh + "px";
+      }
+    }
+  }
+  resize();
+  window.addEventListener("resize", resize, false); // 偵聽事件 resize
+};
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 320,
+  height: 480,
+  backgroundColor: "#000000",
+  pixelArt: true,
+  physics: {
+    default: "matter",
+    matter: {
+      debug: {
+        showBody: true,
+        showStaticBody: true,
+      },
+    },
+  },
+  scene: SceneMatter,
+  parent: "game-content",
+};
+
+const App: React.FC = () => {
+  const gameRef = useRef<any>();
+
+  const setGame = () => {
+    gameRef.current || (gameRef.current = new Phaser.Game(config));
+  };
+
+  useEffect(() => {
+    setGame();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Center w={"100vw"} h={"100vh"} bg={"#000000"}>
+      <Box id="game-content"></Box>
+    </Center>
   );
-}
+};
 
 export default App;
